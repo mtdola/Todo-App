0171beded3c3167c0d7da9b9240fec3a39f7bd60
mtd <mtdolaj@gmail.com>
2019-07-24 00:06:04 +0100

    Started Js

---
+++ main.js
@@ -0,0 +1,9 @@
+var input = document.querySelector("input");
+var btn = document.querySelector("button");
+var ul = document.querySelector("ul");
+
+function btnEvent () {
+	console.log(""Clicked!!)
+}
+
+btn.addEventListener("click", btnEvent);
