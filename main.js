f8ba588cfa8d4fcf88dca798948586d501cd578c
mtd <mtdolaj@gmail.com>
2019-07-24 00:43:05 +0100

    enter key func not working yet

--- main.js
+++ main.js
@@ -2,15 +2,26 @@ var input = document.querySelector("input");
 var btn = document.querySelector("button");
 var ul = document.querySelector("ul");
 
-// function inputLength() {
-// 	return ;
-// }
+function inputLength() {
+	return input.value.length;
+}
 
-btn.addEventListener("click", function (){
-	if (input.value.length > 0) {
-		var li = document.createElement("li");
+function addListItem() {
+	var li = document.createElement("li");
 		li.appendChild(document.createTextNode(input.value));
 		ul.appendChild(li);
+		input.value = "";
 		console.log("Clicked!!");
+}
+
+btn.addEventListener("click", function (){
+	if (inputLength() > 0) {
+		addListItem();
 	}
 });
+
+input.addEventListener("keypress", function(event) {
+	if (inputLength() > 0 && event.keycode === 13) {
+		addListItem();
+	}
+})
