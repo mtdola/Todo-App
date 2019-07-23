3c6ccbcaa039332b3712fc883974cc138bf8caf3
mtd <mtdolaj@gmail.com>
2019-07-24 00:31:40 +0100

    Js working quite fine so far

--- main.js
+++ main.js
@@ -2,8 +2,15 @@ var input = document.querySelector("input");
 var btn = document.querySelector("button");
 var ul = document.querySelector("ul");
 
-function btnEvent () {
-	console.log(""Clicked!!)
-}
+// function inputLength() {
+// 	return ;
+// }
 
-btn.addEventListener("click", btnEvent);
+btn.addEventListener("click", function (){
+	if (input.value.length > 0) {
+		var li = document.createElement("li");
+		li.appendChild(document.createTextNode(input.value));
+		ul.appendChild(li);
+		console.log("Clicked!!");
+	}
+});
